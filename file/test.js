29576b817355a4aa592fd9237a17b72d33aa0f1d
DEBE <lldll2002>
2024-04-15 16:25:56 +0900

    create 2 files and update 2 files

--- file/test.js
+++ file/test.js
@@ -1,7 +1,21 @@
 const fs = require('fs').promises;
 
-fs.writeFile('./file/file.txt','write hello!')
+fs.writeFile('./file.txt','write hello!')
 .catch();
 
-fs.copyFile('./file/file.txt', 'file2.txt')
+fs.copyFile('./file.txt', 'file2.txt')
 .catch(console.error);
+
+// file2.txt에 내용이 있을 수도 있고 없을 수도 있다.
+// 비동기로 처리되기 때문에 줄이 아래 있다고 해서 순서대로 진행된다고 확신 할 수 없다.
+
+// 수정 방법
+// 특정 비동기 처리 후 내용을 수행하려면 then으로 묶어서 처리한다.
+fs.appendFile('./file.txt', 'append hello!')
+  .then(
+    fs.copyFile('./file.txt', 'file3.txt')
+    .catch(console.error)
+  ).catch(console.error)
+
+// 폴더 만들기
+fs.mkdir('new-folder').catch(console.error);
