3149b229f49a0820bb2da312e0061a0c2367e079
DEBE <lldll2002>
2024-04-15 16:39:14 +0900

    delete 2 files, update 1 file and move 1 file

--- file/buffer.js
+++ file/buffer.js
@@ -1,9 +1,33 @@
-const fs = require('fs').promises;
+
+// 숫자의 배열, 데이터의 Byte 그 자체
+const buf = Buffer.from('Hi');
+
+//Buffer 객체 형태로 출력(유니코드 형태)
+console.log(buf);
+
+//Buffer 길이
+console.log(buf.length);
+
+//Buffer 배열 출력
+//배열에 있는 value에 접근하게 되면 ASCII(아스키코드)로 출력
+console.log(buf[0])
+console.log(buf[1])
+//Buffer 정보를 문자열로 출력하고 싶은 경우
+console.log(buf.toString());
+
+// concat 붙이기
 
 
-//option - encoding 설정
-//readFile 함수 상세 정보를 보면 option을 확인 할 수 있음.
-fs.readFile('./text.txt', 'utf8')
-.then(data=>console.log("data2 :", data))
-.catch(console.error)
-.catch((error)=>console.error(error)) // 자바스크립트 문법. 같은 의미임 똑같이 error가 들어가면 둘 다 생략 할 수 있다.
+
+
+const buffer = Buffer.from('저를 버퍼로 바꿔보세요');
+console.log('from():', buffer);
+console.log('length:', buffer.length);
+console.log('toString():', buffer.toString());
+
+const array = [Buffer.from('띄엄'), Buffer.from('띄엄'), Buffer.from('띄어쓰기')];
+const buffer2 = Buffer.concat(array);
+console.log('concat():', buffer2,toString());
+
+const buffer3 = Buffer.alloc(5);
+console.log('alloc():', buffer3);
